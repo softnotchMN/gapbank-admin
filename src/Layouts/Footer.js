40ebb55e3fb54515ae5c5581005a67cad8b0b236
Chukwuemeka Ihedoro <caihedoro@gmail.com>
2023-05-16 03:15:57 +0100

    copyright

--- src/Layouts/Footer.js
+++ src/Layouts/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => {
 			<footer className="footer">
 				<Container fluid>
 					<Row>
-						<Col sm={6}>{new Date().getFullYear()} © Velzon.</Col>
+						<Col sm={6}>{new Date().getFullYear()} © Gap Finance.</Col>
 						<Col sm={6}>
-							<div className="text-sm-end d-none d-sm-block">
+							{/* <div className="text-sm-end d-none d-sm-block">
 								Design & Develop by Themesbrand
-							</div>
+							</div> */}
 						</Col>
 					</Row>
 				</Container>
